9e2a4a5790e5e4ed3d93ce1d713a499a45449c45
Deven Prajapati <devenprajapati2307@gmail.com>
2025-06-30 23:04:42 +0530

    Data Transformation Done!

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,8 +4,13 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transform import DataTransformation
+from src.components.data_transform import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -18,7 +23,7 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or commponent")
+        logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv('notebook/data/stud.csv')
             logging.info("Read the dataset as dataframe")
@@ -45,5 +50,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
   
+    data_transform = DataTransformation()
+    data_transform.initiate_data_transformation(train_data, test_data)
